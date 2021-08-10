a18d43180e62158f96afda91e30ed0ab84f00332
Gustavo <gustavohs2004@gmail.com>
2021-08-10 11:50:19 -0300

    Tela procurar pronta

---
+++ ListaLogin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaMercado
+{
+    class ListaLogin
+    {
+        public string Usuario { get; set; }
+        public string Senha { get; set; }
+
+        public ListaLogin(string usuario, string senha)
+        {
+            Usuario = usuario;
+            Senha = senha;
+        }
+    }
+}
--- TelasLogin/FuncoesTelaLogin/TelaProcurar.cs
+++ TelasLogin/FuncoesTelaLogin/TelaProcurar.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,47 @@ namespace SistemaMercado.TelasLogin.FuncoesTelaLogin
         public TelaProcurar()
         {
             InitializeComponent();
+        }
+
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+            TelaGerenciarLogins tela = new TelaGerenciarLogins();
+            this.Visible = false;
+            tela.Show();
+        }
+
+        private void TelaProcurar_Load(object sender, EventArgs e)
+        {
+            List<string> usuarios = new List<string>();
+            string select = $"SELECT usuario from dbo.Usuarios";
+            SqlCommand cmd = new SqlCommand(select, DBConnection.Connection);
+            DBConnection.Connection.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                usuarios.Add(dr["usuario"].ToString());
+            }
+            cboOpcoes.DataSource = usuarios;
+            DBConnection.Connection.Close();
+        }
+
+        private void txtProcurar_Click(object sender, EventArgs e)
+        {
+            if (cckListar.Checked)
+            {
+                List<ListaLogin> usuarios = new List<ListaLogin>();
+                string select = $"SELECT * from dbo.Usuarios";
+                SqlCommand cmd = new SqlCommand(select, DBConnection.Connection);
+                DBConnection.Connection.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    ListaLogin lista = new ListaLogin(dr["usuario"].ToString(), dr["senha"].ToString());
+                    usuarios.Add(lista);
+                }
+                dgvUsuarios.DataSource = usuarios;
+                DBConnection.Connection.Close();
+            }
         }
     }
 }
